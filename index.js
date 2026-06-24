50d2bd86b72500ccc8bee08fdfeb7686baa79060
EmmanuelUkaegbu <eukaegbu005@gmail.com>
2026-06-24 21:09:20 +0100

    updated backend

--- index.js
+++ index.js
@@ -12,12 +12,15 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World your Node API  server is Live emeka");
 });
+app.use(cors());
 app.use("/api/auth", userRoute);
 app.use("/api/product", productRoute);
 app.listen(PORT, () => {
